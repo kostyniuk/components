d9fb028ad715df721bc59949eddfc4ea679de87a
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-04-22 23:23:34 +0200

    feat: add TabSwitch component and card UI elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { TabSwitch } from "@/components/custom/tab-switch"
 import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose px-[100px]">
         <div>
           <h1 className="font-medium">Project ready!</h1>
           <p>You may now add components and start building.</p>
@@ -13,6 +14,7 @@ export default function Page() {
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
         </div>
+        <TabSwitch />
       </div>
     </div>
   )
